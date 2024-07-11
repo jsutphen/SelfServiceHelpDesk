12961f4bc3d1fcffc99115cd72be7fb3ab882517
jsutphen <jonathan@sutphen.de>
2024-07-11 22:48:58 +0200

    change routing

--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,30 @@ router.get('/new-ticket', (req, res, next) =>  {
 });
 
 router.post('/new-ticket', asyncHandler(async(req, res, next) => {
-  const name = req.body.firstName + req.body.lastName;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
   const email = req.body.email;
-  const ticket = new Ticket({ name: name, email: email});
+  const ticket = new Ticket({ 
+    firstName: firstName, 
+    lastName: lastName,
+    email: email
+  });
   let id;
   await ticket.save().then( savedTicket =>
     id = savedTicket.id
   );
   // prettify id
+  // id is very, very likely also unique with only 14 characters
+  // as it is made up of 4 bytes of timestamp and additional 
+  // random bytes
   id = id.substring(0,4) + '-' + 
     id.substring(5,9) + '-' + id.substring(10,14);
   res.render('submissionComplete', { id: id });
+}));
+
+router.get('/tickets', asyncHandler(async (req, res, next) => {
+  const tickets = await Ticket.find({});
+  res.render('ticketsDashBoard', { tickets: tickets });
 }));
 
 module.exports = router;
