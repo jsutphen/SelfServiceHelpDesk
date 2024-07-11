b36f9b09f7f2c6ac1a9ef1a6cd5b1f2b696576d8
jsutphen <jonathan@sutphen.de>
2024-07-11 21:59:47 +0200

    create Ticket Schema

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,5 +1,11 @@
+const mongoose = require("mongoose");
+
 const ticketSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   time: { type: Date, required: true, default: Date.now() }
 })
+
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+module.exports = Ticket
