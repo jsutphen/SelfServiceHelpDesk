a3a3f89e8fe1bf7acb9b6f67807118713a000007
jsutphen <jonathan@sutphen.de>
2024-07-11 22:00:07 +0200

    provide routing logic for new tickets form

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 var express = require('express');
+const asyncHandler = require('express-async-handler');
 var router = express.Router();
+const Ticket = require('../models/Ticket');
 
 /* GET home page. */
-router.get('/', (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 router.get('/new-ticket', (req, res, next) =>  {
   res.render('newTicket');
 });
+
+router.post('/new-ticket', asyncHandler(async(req, res, next) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const ticket = new Ticket({ name: name, email: email});
+  let id;
+  await ticket.save().then( savedTicket =>
+    id = savedTicket.id
+  );
+  // prettify id
+  id = id.substring(0,4) + '-' + 
+    id.substring(5,9) + '-' + id.substring(10,14);
+  res.render('submissionComplete', { id: id });
+}));
 
 module.exports = router;
