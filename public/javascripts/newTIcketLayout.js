6e0bd4cee23b2a9ce0b396d9d06956d6167004ec
jsutphen <jonathan@sutphen.de>
2024-07-21 01:35:25 +0200

    create layout control script for new ticket page

---
+++ public/javascripts/newTIcketLayout.js
@@ -0,0 +1,55 @@
+function handleTicketTypeChange(ticketType) {
+  if (ticketType === 'pleaseChoose') {
+    pleaseChoose_layout();
+  }
+  if (ticketType == 'hardwareInstallation') {
+    hardwareInstallationLayout();
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  initLayout();
+});
+
+function initLayout() {
+  // layout
+  const contactFields = document.querySelector('#contactFields')
+  contactFields.style.display = 'none';
+
+  const hardwareInstallationFields = document.querySelector('#hardwareInstallationFields');
+  hardwareInstallationFields.style.display = 'none';
+
+  const form_submit = document.querySelector('form button');
+  form_submit.style.display = 'none';
+
+  const ticketTypeSelector = document.querySelector('#ticketType');
+  ticketTypeSelector.value = 'pleaseChoose';
+  
+  // logic
+  ticketTypeSelector.addEventListener('change', () => {
+    handleTicketTypeChange(ticketTypeSelector.value);
+  });
+
+  handleTicketTypeChange();
+}
+
+function pleaseChoose_layout() {
+  initLayout();
+}
+
+function hardwareInstallationLayout() {
+  initLayout();
+
+  const ticketTypeSelector = document.querySelector('#ticketType');
+  ticketTypeSelector.value = 'hardwareInstallation';
+
+  const contactFields = document.querySelector('#contactFields')
+  contactFields.style.display = 'block';
+  
+  const hardwareInstallationFields = document.querySelector('#hardwareInstallationFields');
+  hardwareInstallationFields.style.display = 'block';
+
+  const form_submit = document.querySelector('form button');
+  form_submit.style.display = 'block';
+}
