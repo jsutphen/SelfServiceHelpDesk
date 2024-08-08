3c40b11dfc435b22906b5c042bffaddc3c52b245
jsutphen <jonathan@sutphen.de>
2024-08-08 20:58:43 +0200

    new dbscript

--- dbscript.js
+++ dbscript.js
@@ -1,20 +1,28 @@
 const mongoose = require('mongoose');
-const TicketType = require('./models/TicketType');
+const { TicketType } = require('./models/TicketType');
+const { FieldType } = require('./models/FieldType');
 require('dotenv').config();
 
 async function main() {
   await mongoose.connect(process.env.DB);
-  const hardwareInstallation = new TicketType.TicketType({
-    typeName: 'Hardware-Installation',
+
+  const hardwareInstallationTicketType = new TicketType({
     shortName: 'hardwareInstallation',
-    additionalFieldNames: [
-      'Alter Standort',
-      'Neuer Standort',
-      'Datum',
-    ],
+    typeName: 'Hardware-Installation',
+    additionalFieldTypes: [],
   });
-  await hardwareInstallation.save();
-  
+
+  const oldRoomField = await FieldType.findOne({ name: 'Alter Raum' });
+  hardwareInstallationTicketType.additionalFieldTypes.push(oldRoomField.id);
+
+  const newRoomField = await FieldType.findOne({ name: 'Neuer Raum' });
+  hardwareInstallationTicketType.additionalFieldTypes.push(newRoomField.id);
+
+  const date = await FieldType.findOne({ name: 'Datum' });
+  hardwareInstallationTicketType.additionalFieldTypes.push(date.id);
+
+  await hardwareInstallationTicketType.save();
+
   // await hardwareInstallation.save();
   await mongoose.disconnect();
 }
