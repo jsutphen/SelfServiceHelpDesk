63c54edf1720eeaba41d9274fcbbd87d532476df
jsutphen <jonathan@sutphen.de>
2024-08-08 21:01:26 +0200

    add fieldTypes to ticketType Schema instead of fieldNames

--- models/TicketType.js
+++ models/TicketType.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+require('./FieldType');
 
 const ticketTypeSchema = new mongoose.Schema(
   {
+    // shortName appears in the URL of the ticket
     shortName: { type: String, required: true },
+    // typeName is the full display name of the ticket type
     typeName: { type: String, required: true },
-    additionalFieldNames: [{ type: String }],
+    additionalFieldTypes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'FieldType' }],
   },
 );
 
