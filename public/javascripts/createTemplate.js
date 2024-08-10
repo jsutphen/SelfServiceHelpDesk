1e156e95ff4cc1767a9860e5c08e2bc544aad4af
jsutphen <jonathan@sutphen.de>
2024-08-11 01:44:19 +0200

    reorder and change code for client-side button behaviour

--- public/javascripts/createTemplate.js
+++ public/javascripts/createTemplate.js
@@ -1,18 +1,29 @@
 const newFieldButton = document.querySelector('#addFields');
+let newFieldsIndex = 0;
 newFieldButton.addEventListener('click', () => {
   const newFieldsContainer = document.querySelector('#newFieldsContainer');
+
+  // name field for new additionalField
   const name = document.createElement('input');
+  name.name = 'additionalFieldTypesNames';
   name.placeholder = 'Feldname';
-  const select = document.createElement('select');
-  select.name = 'additionalFieldTypes';
-  if (!Array.isArray(fieldTypes)) {
-    fieldTypes = [fieldTypes];
-  }
-  fieldTypes.forEach(fieldType => {
-    const option = document.createElement('option');
-    option.innerHTML = `${fieldType.name}, ${fieldType.type}`;
-    select.append(option);
-    option.value = fieldType._id;
-  });
-  newFieldsContainer.append(select);
+  newFieldsContainer.append(name);
+
+  // html type of the additionalField
+  const typeSelect = document.createElement('select');
+  typeSelect.name = 'additionalFieldTypesTypes';
+
+  const optionText = document.createElement('option');
+  optionText.value = 'text';
+  optionText.innerHTML = 'Text';
+  typeSelect.append(optionText);
+
+  const optionDate = document.createElement('option');
+  optionDate.value = 'date';
+  optionDate.innerHTML = 'Datum';
+  typeSelect.append(optionDate);
+
+  newFieldsContainer.append(typeSelect);
+
+  newFieldsIndex += 1;
 });
