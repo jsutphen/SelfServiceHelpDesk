15ca18b9a4ce19310e60928ab5acf07a0f936dd1
jsutphen <jonathan@sutphen.de>
2024-08-26 19:16:31 +0200

    fix incorrect 404 error handling

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use((next) => {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
