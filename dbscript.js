41a8e783e21258eee3d7be32defb6167e7205eae
jsutphen <jonathan@sutphen.de>
2024-08-26 19:16:54 +0200

    different dbscript

--- dbscript.js
+++ dbscript.js
@@ -6,24 +6,10 @@ require('dotenv').config();
 async function main() {
   await mongoose.connect(process.env.DB);
 
-  const hardwareInstallationTicketType = new TicketType({
-    shortName: 'hardwareInstallation',
-    typeName: 'Hardware-Installation',
-    additionalFieldTypes: [],
-  });
+  const textarea = await FieldType.findOneAndDelete({ name: 'Text (mehrere Zeilen)' });
 
-  const oldRoomField = await FieldType.findOne({ name: 'Alter Raum' });
-  hardwareInstallationTicketType.additionalFieldTypes.push(oldRoomField.id);
+  
 
-  const newRoomField = await FieldType.findOne({ name: 'Neuer Raum' });
-  hardwareInstallationTicketType.additionalFieldTypes.push(newRoomField.id);
-
-  const date = await FieldType.findOne({ name: 'Datum' });
-  hardwareInstallationTicketType.additionalFieldTypes.push(date.id);
-
-  await hardwareInstallationTicketType.save();
-
-  // await hardwareInstallation.save();
   await mongoose.disconnect();
 }
 main().catch((err) => console.log(err));
