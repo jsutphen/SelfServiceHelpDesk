b87315ad28ebfa7e271e7e048f31b9ef028b2a46
jsutphen <jonathan@sutphen.de>
2024-08-30 11:22:56 +0200

    dbscript

--- dbscript.js
+++ dbscript.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 const { TicketType } = require('./models/TicketType');
 const { FieldType } = require('./models/FieldType');
+const User = require('./models/User');
 require('dotenv').config();
 
 async function main() {
   await mongoose.connect(process.env.DB);
 
-  const textarea = await FieldType.findOneAndDelete({ name: 'Text (mehrere Zeilen)' });
+  const jonathan = await User.findOne({ username: 'jonathan' });
 
-  
+  console.log(jonathan);
 
   await mongoose.disconnect();
 }
