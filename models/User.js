21412fb2e548bcecab7ace54c72275e750b44ae5
jsutphen <jonathan@sutphen.de>
2024-08-30 11:25:02 +0200

    add admin property to user, restrict critical actions, add user management

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean },
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
