4305bf672fc56f11cbd5468121866ce883454e4a
jsutphen <jonathan@sutphen.de>
2024-09-03 20:58:45 +0200

    dbscript

--- dbscript.js
+++ dbscript.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const { TicketType } = require('./models/TicketType');
 const { FieldType } = require('./models/FieldType');
+const Ticket = require('./models/Ticket');
 const User = require('./models/User');
 require('dotenv').config();
 
 async function main() {
   await mongoose.connect(process.env.DB);
 
-  const jonathan = await User.findOne({ username: 'jonathan' });
-
-  console.log(jonathan);
+  const allTickets = await Ticket.find();
+  allTickets.forEach((element) => {
+    console.log(element.ticketType.equals('66c50e6cf2ffbd20ec3888b3'));
+  });
 
   await mongoose.disconnect();
 }
